0d4d9822dfa0725d405c101c4dd5408d175c61c3
duongwthuj <thunguyenduong37@gmail.com>
2025-03-19 02:04:50 +0700

    SetUp Utlis: Colors, Enums, image, string, sizes

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:thuctapcoso/utlis/theme/theme.dart';
+import 'app.dart';
 
-void main() {
-  runApp(MyApp());
+void main(){
+  runApp(const App());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  bool isDarkMode = false;
-
-  void toggleTheme() {
-    setState(() {
-      isDarkMode = !isDarkMode;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: isDarkMode ? TAppTheme.darkTheme : TAppTheme.lightTheme,
-      home: Scaffold(
-        appBar: AppBar(title: Text("Chuyển đổi Theme")),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Chế độ hiện tại: ${isDarkMode ? "Dark" : "Light"}",
-                style: Theme.of(context).textTheme.bodyLarge,
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: toggleTheme,
-                child: Text("Chuyển đổi Theme"),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
---
+++ lib/utlis/constants/enums.dart
@@ -0,0 +1,18 @@
+enum TextSizes {
+  small,
+  medium,
+  large,
+}
+
+enum OrderStatus {
+  processing,
+  shipped,
+  delivered,
+}
+
+enum PaymentMethod{
+  paypal,
+  googlePay,
+  creditCard,
+  cashOnDelivery,
+}
